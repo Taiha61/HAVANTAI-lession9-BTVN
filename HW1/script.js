593cbbcd25ca0295b0dc1a75e99620e88dbdef3a
taiha61 <havantai6195@gmail.com>
2022-01-11 00:32:59 +0700

    BTVN-9

--- HW1/script.js
+++ HW1/script.js
@@ -7,18 +7,54 @@
 //     }
 //   }
 
-function checkAge(age) {
-    return (age > 18) ? true : `Did parents allow you?`;
-}
-console.log(checkAge(20));
+// function checkAge(age) {
+//     return (age > 18) ? true : `Did parents allow you?`;
+// }
+// console.log(checkAge(20));
 
 // HW2 tìm số nhỏ nhất
-function Min(a, b) {
-    if (a < b) {
-        let min = a;
+// function checkMin(a, b) {
+//     return (a < b && a !== b) ? a : b;
+// }
+// console.log(checkMin(25, 15));
+// // HW3
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+// let ask = (question, yes, no) => {
+//     if (confirm(question)) yes()
+//     else no();
+// }
+
+// ask(
+//     "Do you agree?",
+//     function() { alert("You agreed."); },
+//     function() { alert("You canceled the execution."); }
+// );
+//HW4 
+let userName = prompt("Nhập tên đăng nhập");
+let passWords;
+
+function checkDangNhap(userName) {
+    if (userName === "Admin") {
+        passWords = prompt("nhập mật khẩu");
+        enterPassWords(passWords);
+    } else if (userName === ``) {
+        alert("Canceled");
     } else {
-        let min = b;
+        alert("Tôi không biết bạn")
     }
-    return min;
 }
-// HW3
+
+function enterPassWords(passWords) {
+    if (passWords === "cafedev") {
+        alert("Chào Mừng!")
+    } else if (passWords === ``) {
+        alert("Đã hủy")
+    } else {
+        alert("Mật khẩu sai")
+    }
+}
+checkDangNhap(userName);
